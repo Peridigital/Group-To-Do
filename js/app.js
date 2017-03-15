0753ddd5d7a2f42ef75ad130d03693c4ed50887f
sbrycebarker <sbrycesti@gmail.com>
2017-03-15 11:12:25 -0600

    first

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
-angular.module('groupToDo',[]);
+angular.module('groupToDo', ['ui.router'])
+
+  .config( function ($urlRouterProvider, $stateProvider){
+
+$urlRouterProvider.when('', '/');
+
+  $stateProvider
+    .state('welcome', {
+      templateUrl: 'welcome.html',
+      contoller: 'welcomeCtrl',
+      url: '/'
+    })
+
+    .state('to-do', {
+      templateUrl: 'todo.html' ,
+      controller: 'todoCtrl',
+      url: '/todo'
+    })
+
+    .state('archive', {
+      templateUrl: 'archive.html'
+      controller: 'archiveCtrl',
+      url: 'archive'
+    })
+
+}
