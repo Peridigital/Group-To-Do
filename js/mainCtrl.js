b35546092ffd0f55aa992ba1bd3bba16b79982c0
Aaron Bowers <aaronjbowers@gmail.com>
2017-03-15 10:34:02 -0600

    initial push

---
+++ js/mainCtrl.js
@@ -0,0 +1,3 @@
+angular.module('groupToDo').controller('mainCtrl', function($scope, toDoService) {
+  $scope.test = service.serviceTest
+})
---
+++ js/service.js
@@ -0,0 +1,3 @@
+angular.module('groupToDo').service('toDoService', function() {
+  this.serviceTest = 'Hello, I am a working test!'
+})
