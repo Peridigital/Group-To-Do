869071d51e8fac3b81f3eeb50aac2a427e4fff7a
Aaron Bowers <aaronjbowers@gmail.com>
2017-03-15 11:22:49 -0600

    added base functionality

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,3 +1,25 @@
 angular.module('groupToDo').controller('mainCtrl', function($scope, toDoService) {
-  $scope.test = toDoService.serviceTest
+  //Array of all to do items
+  $scope.toDoArray = [];
+  $scope.archivedToDoArray = [];
+
+
+  //Calls toDoService's function to make an item, then pushes it to toDoArray
+  $scope.addNewToDo = function (name, description) {
+    $scope.toDoArray.push(toDoService.newToDoItem(name, description))
+  }
+  //Test call
+  $scope.addNewToDo('Test!', 'Hello, I am a test!')
+
+
+ //Removes selected item from toDoArray
+  $scope.removeToDo = function (index) {
+    $scope.toDoArray.splice(index, 1)
+  }
+
+  $scope.archiveToDo = function (index) {
+    $scope.archivedToDoArray.push($scope.toDoArray[index])
+    $scope.removeToDo(index)
+
+  }
 })
